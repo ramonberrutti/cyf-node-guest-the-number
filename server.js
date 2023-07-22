91c763ffef698627b5d0a4b33b93fa097dcf43ec
Ramón Berrutti <ramonberrutti@gmail.com>
2023-07-22 12:04:13 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/guest', (req, res) => {
+    const num = Math.floor(Math.random() * 10);
+    const guessedNum = req.query.num || 0;
+
+    res.send({
+        "number": num,
+        "guessed_number": guessedNum,
+        "result": guessedNum == num
+    });
+});
+
+
+app.listen(process.env.PORT || 3000);
